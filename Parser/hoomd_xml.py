13e61adf2092f78df280d0e8c054218a3346887a
Shirui <Shirui816@outlook.com>
2016-04-16 15:31:07 +0800

    CRLF->LF

--- Parser/hoomd_xml.py
+++ Parser/hoomd_xml.py
@@ -1,21 +1,19 @@
 from numpy import array, loadtxt
 import numpy as np
 from io import StringIO
-import xml.etree.cElementTree as ET  # pypy will be a bit slower than python
+import xml.etree.cElementTree as ET  #pypy will be a bit slower than python
 import warnings
 from DataStructure.DtypeDict import dtypeDict
 try:
     from iopro import loadtxt
 except ImportError:
     warnings.warn("No module iopro, I can't accelerate while files are large.")
-
 class hoomd_xml(object):
     @staticmethod
     def _get_attrib(dd):
         dt = eval('[' + ','.join(["('%s', int)" % key for key in dd.keys()]) + ']')
         values = [tuple(dd.values())]
         return array(values, dtype=dt)
-
     def __init__(self, filename):
         tree = ET.ElementTree(file=filename)
         root = tree.getroot()
@@ -33,8 +31,6 @@ class hoomd_xml(object):
                 self.nodes[e.tag] = loadtxt(StringIO(e.text), dtype=dt)
             except KeyError:
                 self.nodes[e.tag] = loadtxt(StringIO(e.text))
-
-
 if __name__ == "__main__":
     from sys import argv
     file = argv[1]
