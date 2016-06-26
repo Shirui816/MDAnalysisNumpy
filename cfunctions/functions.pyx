5fce6717ef30d14583e693ee79333688d5905af7
Shirui <Shirui816@outlook.com>
2016-06-26 18:43:14 +0800

    modify data types

--- cfunctions/functions.pyx
+++ cfunctions/functions.pyx
@@ -43,4 +43,28 @@ def RgRadial(np.ndarray r_pos, np.ndarray box):
                         dr = dx*dx+dy*dy+dz*dz
                         rg2 += sx*sx+sy*sy+sz*sz
                         rgn2 += (sx*dx+sy*dy+sz*dz)**2 / dr
-        return(rg2/m**2, rgn2/m**2) 
+        return(rg2/m**2, rgn2/m**2)
+
+@cython.boundscheck(False) # turn off bounds-checking for entire function
+@cython.wraparound(False)  # turn off negative index wrapping for entire function
+def RgRadial_eff_idx(np.ndarray[double, ndim=2] r_pos, np.ndarray[double, ndim=1] box):
+        cdef long m = r_pos.shape[0]
+        cdef double rg2 = 0
+        cdef double rgn2 = 0
+        cdef long i
+        cdef long j
+        cdef double dx, dy, dz, sx, sy, sz, dr
+        for i in range(m-1):
+                for j in range(i+1, m):
+                        #d = pbc1d((r_pos[i]+r_pos[j])/2, box)
+                        dx = pbc(r_pos[i,0] + r_pos[j,0]/2, box[0])
+                        dy = pbc(r_pos[i,1] + r_pos[j,1]/2, box[1])
+                        dz = pbc(r_pos[i,2] + r_pos[j,2]/2, box[2])
+                        sx = pbc(r_pos[i,0]-r_pos[j,0], box[0])
+                        sy = pbc(r_pos[i,1]-r_pos[j,1], box[1])
+                        sz = pbc(r_pos[i,2]-r_pos[j,2], box[2])
+                        #s = pbc1d(r_pos[i]-r_pos[j], box)
+                        dr = dx*dx+dy*dy+dz*dz
+                        rg2 += sx*sx+sy*sy+sz*sz
+                        rgn2 += (sx*dx+sy*dy+sz*dz)**2 / dr
+        return(rg2/m**2, rgn2/m**2)
