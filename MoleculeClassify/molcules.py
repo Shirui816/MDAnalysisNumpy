c9f43345394e4be3412d30361a8829622d72000a
施呵呵 <shirui816@gmail.com>
2016-11-21 14:29:06 +0800

    Update

--- MoleculeClassify/molcules.py
+++ MoleculeClassify/molcules.py
@@ -24,7 +24,7 @@ def grabmolecules_without_body(i, body_hash, bond_hash, mol_idxes,  mol_used):
     for j in bond_hash[i]:
         grabmolecules_without_body(j,body_hash, bond_hash, mol_idxes, mol_used)
 
-def grab_iter(i, bond_hash, mol_used, body_hash=None):
+def grab_iter_dual(i, bond_hash, mol_used, body_hash=None):
     S = [i]
     R = []
     while S:
@@ -32,8 +32,9 @@ def grab_iter(i, bond_hash, mol_used, body_hash=None):
         if mol_used[v] == False:
             R.append(v)
             mol_used[v] = True
-            for w in bond_hash[v]:
-                S.append(w)
+            #for w in bond_hash[v]:
+                #S.append(w)
+            S.extend(bond_hash[v])
             if not body_hash:
                 continue
             for w in body_hash[v]:
@@ -41,3 +42,21 @@ def grab_iter(i, bond_hash, mol_used, body_hash=None):
                 for x in bond_hash[w]:
                     S.append(x)
     return R
+
+def grab_iter_uni(i, bond_hash, body_hash):
+    seen = {i}
+    stack = [i]
+    result = []
+    if not body_hash:
+        body_hash = {}
+    while stack:
+        x = stack[-1]
+        nxt = set(bond_hash.get(x, []))|set(body_hash.get(x, [])) - seen
+        if nxt:
+            #stack.extend(list(nxt))
+            stack.extend(nx for nx in bond_hash[x] if nx in nxt) # Ordered
+            seen |= nxt
+        else:
+            result.append(x)
+            stack.pop()
+    return result
